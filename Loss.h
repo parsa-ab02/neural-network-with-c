ab8e29790c3ca225ee8ad124bf612d3623198413
parsa-ab02 <parsaabedi02@gmail.com>
2026-06-28 21:28:08 +0330

    add loss functions

--- Loss.h
+++ Loss.h
@@ -2,6 +2,8 @@
 #include <math.h>
 #include "matrix.h"
 
+const double eps = 1e-15;
+
 // Linear Regression loss functions : --------------------
 
 typedef struct
@@ -10,24 +12,140 @@ typedef struct
 }Loss;
 
 
-double Mean_Squared_Error(Matrix matrix){
-    
+double Mean_Squared_Error(const Matrix *RealOutput, const Matrix *predictedOutput){
+    if(RealOutput->n_cols != 1 || predictedOutput->n_cols != 1){
+        printf("real output or predicted output are not a vector\n");
+        return 0.0;
+    }
+    if(RealOutput->n_rows != predictedOutput->n_rows){
+        printf("real output and predicted out put are not the same size\n");
+        return 0.0;
+    }
+    int size = RealOutput->n_rows;
+    double result = 0;
+
+    for(size_t i = 0; i < size; i++){
+        double error = *At(RealOutput, i , 0)-*At(predictedOutput, i ,0);
+        result += error * error;
+    }
+
+    return result/size;
 }
 
-double Mean_Absoult_Error(){
+double Mean_Absoult_Error(const Matrix *RealOutput, const Matrix *predictedOutput){
+    if(RealOutput->n_cols != 1 || predictedOutput->n_cols != 1){
+        printf("real output or predicted output are not a vector\n");
+        return 0.0;
+    }
+    if(RealOutput->n_rows != predictedOutput->n_rows){
+        printf("real output and predicted out put are not the same size\n");
+        return 0.0;
+    }
+    int size = RealOutput->n_rows;
+    double result = 0;
 
+    for(size_t i = 0; i < size; i++){
+        result += fabs(*At(RealOutput, i , 0)-*At(predictedOutput, i ,0));
+    }
+
+    return result/size;
 }
 
-double Huber_Loss(){
+double Huber_Loss(const Matrix *RealOutput, const Matrix *predictedOutput, double delta){
+    if(RealOutput->n_cols != 1 || predictedOutput->n_cols != 1){
+        printf("real output or predicted output are not a vector\n");
+        return 0.0;
+    }
+    if(RealOutput->n_rows != predictedOutput->n_rows){
+        printf("real output and predicted out put are not the same size\n");
+        return 0.0;
+    }
+    int size = RealOutput->n_rows;
+    double result = 0;
 
+    for(size_t i = 0; i < size; i++){
+        double error = fabs(*At(RealOutput, i , 0)-*At(predictedOutput, i ,0));
+        if(error <= delta){
+            result += (0.5*error * error);
+        }else{
+            result += delta*(error - 0.5*delta);
+        }
+    }
+
+    return result/size;
 }
 
 // classification loss functions : --------------------
 
-void Binary_Cross_Entropy_Loss(){
+double Binary_Cross_Entropy_Loss(const Matrix *RealOutput, const Matrix *predictedOutput){
+    if(RealOutput->n_cols != 1 || predictedOutput->n_cols != 1){
+        printf("real output or predicted output are not a vector\n");
+        return 0.0;
+    }
+    if(RealOutput->n_rows != predictedOutput->n_rows){
+        printf("real output and predicted out put are not the same size\n");
+        return 0.0;
+    }
+    int size = RealOutput->n_rows;
+    double result = 0;
 
+    for(size_t i = 0; i < size; i++){
+        double p = *At(predictedOutput, i, 0);
+        if(p < 0 || p > 1){
+            printf("probabilities are invalid! it must be  between 0 to 1\n");
+            return 0.0;
+        }
+        if (p < eps){
+            p = eps;
+        }
+        if (p > 1.0 - eps){
+            p = 1.0 - eps;
+        }
+        double error = *At(RealOutput, i , 0) * log(p) + (1 - *At(RealOutput, i , 0)) * log(1 - p);
+        result += error;
+    }
+
+    return -result/size;
 }
 
-void Sparse_Categorical_Cross_Entropy_Loss(){
+double Sparse_Categorical_Cross_Entropy_Loss(const Matrix *RealOutput, const Matrix *predictedOutput){
+    if(RealOutput->n_cols != 1){
+        printf("real output is not a vector\n");
+        return 0.0;
+    }
+    if(RealOutput->n_rows != predictedOutput->n_rows){
+        printf("sizes do not match\n");
+        return 0.0;
+    }
+    int size = RealOutput->n_rows;
+    double result = 0;
 
+    for(size_t i = 0; i < size; i++){
+        int index = (int)*At(RealOutput, i, 0);
+        if(index != *At(RealOutput, i, 0)){
+            printf("output vector is invalid! its type is not int\n");
+            return 0.0;
+        }
+        if(index >= predictedOutput->n_cols){
+            printf("output vector is invalid! its indexing doesn't match the size of predictions matrix\b");
+            return 0.0;
+        }
+
+        double p = *At(predictedOutput, i, index);
+        if(p < 0 || p > 1){
+            printf("probabilities are invalid! it must be  between 0 to 1\n");
+            return 0.0;
+        }
+        if (p < eps){
+            p = eps;
+        }
+        if (p > 1.0 - eps){
+            p = 1.0 - eps;
+        }
+
+        double error = -log(p);
+        result += error;
+    }
+
+    return result/size;
 }
